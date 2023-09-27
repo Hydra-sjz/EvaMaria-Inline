a0d1381f2bc5141a296b9cc17c479f81d3d96f64
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-09-27 19:00:56 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -2,34 +2,17 @@ import logging
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
+
 from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, CUSTOM_FILE_CAPTION
+from utils import get_size
+from info import CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
-cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
-async def inline_users(query: InlineQuery):
-    if AUTH_USERS:
-        if query.from_user and query.from_user.id in AUTH_USERS:
-            return True
-        else:
-            return False
-    if query.from_user and query.from_user.id not in temp.BANNED_USERS:
-        return True
-    return False
 
 @Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
-    
-    if not await inline_users(query):
-        await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='Hmm why?',
-                           switch_pm_parameter="hehe")
-        return
-
     results = []
     if '|' in query.query:
         string, file_type = query.query.split('|', maxsplit=1)
@@ -67,13 +50,12 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
+        switch_pm_text = f"🎵 Results - {total}"
         if string:
             switch_pm_text += f" for {string}"
         try:
             await query.answer(results=results,
                            is_personal = True,
-                           cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
@@ -88,7 +70,6 @@ async def answer(bot, query):
 
         await query.answer(results=[],
                            is_personal = True,
-                           cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="okay")
 
@@ -96,7 +77,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🔍bSearch Again.', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🔍 Search Again.', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
